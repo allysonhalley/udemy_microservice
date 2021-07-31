f7c4ffd20925f0f9545d6b2a677272d7d1001ad7
Allyson Halley <allysonhalley@gmail.com>
2021-07-31 20:34:26 -0300

    project hrworker created

---
+++ hrworker/src/main/java/com/ms_course/hrworker/HrworkerApplication.java
@@ -0,0 +1,13 @@
+package com.ms_course.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrworkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrworkerApplication.class, args);
+	}
+
+}
