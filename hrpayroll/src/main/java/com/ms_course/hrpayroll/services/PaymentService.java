043e414ff5c76af70ad829bb329d1b4826fa5072
Allyson Halley <allysonhalley@gmail.com>
2021-08-01 16:52:22 -0300

    comunication inter projects ok

--- hrpayroll/src/main/java/com/ms_course/hrpayroll/services/PaymentService.java
+++ hrpayroll/src/main/java/com/ms_course/hrpayroll/services/PaymentService.java
@@ -1,14 +1,33 @@
 package com.ms_course.hrpayroll.services;
 
-import com.ms_course.hrpayroll.entities.Payment;
+import java.util.HashMap;
+import java.util.Map;
 
+import com.ms_course.hrpayroll.entities.Payment;
+import com.ms_course.hrpayroll.entities.Worker;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PaymentService {
 
+	@Value("${hrworker.host}")
+	private String workerHost;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
 	public Payment getPayment(long workerId, int days){
-		return new Payment("Dudu", 500.0, days);
+
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerId);
+
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 	
 }
