3736d22e2088d51f7681674b717a558d33896dae
Allyson Halley <allysonhalley@gmail.com>
2021-08-06 17:00:30 -0300

    projeto hreurekaserve created

---
+++ hreurekaserver/src/main/java/com/ms_course/hreurekaserver/HreurekaserverApplication.java
@@ -0,0 +1,13 @@
+package com.ms_course.hreurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HreurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HreurekaserverApplication.class, args);
+	}
+
+}
---
+++ hreurekaserver/src/test/java/com/ms_course/hreurekaserver/HreurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms_course.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HreurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
