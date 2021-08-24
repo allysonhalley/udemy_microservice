de0762ede020ac6eda1c664418bd3a61b85038fc
Allyson Halley <allysonhalley@gmail.com>
2021-08-24 17:30:16 -0300

    project zuul created

---
+++ hrapigatewayzuul/src/test/java/com/ms_course/hrapigatewayzuul/HrapigatewayzuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms_course.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrapigatewayzuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
